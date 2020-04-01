3ae9dd8e914d75607dd9f410810a5dc9cfc72a4e
Erik Cokeley <erik.cokeley@ring.com>
2020-03-31 23:12:32 -0400

    Added foo function for testing.

--- src/hello-world.c
+++ src/hello-world.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+int main(int, char **);
+
+void foo() {
+	int a = 4;
+	printf("foo has a lot of variadic food %d", a);
+}
+
 int main(int argc, char ** argv) {
 	printf("Hello World!\n");
 	return 0;
